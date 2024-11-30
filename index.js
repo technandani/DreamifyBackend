884b15e1dc6852f38d1b7ebc67149dc5c344917d
Nandani singh <149961483+technandani@users.noreply.github.com>
2024-12-01 00:20:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false })); 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://dreamify-sigma.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://dreamify-sigma.vercel.app/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
